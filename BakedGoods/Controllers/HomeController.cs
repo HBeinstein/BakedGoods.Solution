b9160583fa51b984edeed6f048c037e8f8fe9b5f
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-08-14 15:26:44 -0700

    add viewbags to home controller to display all items on home/index

--- BakedGoods/Controllers/HomeController.cs
+++ BakedGoods/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using System.Reflection.PortableExecutable;
 using Microsoft.AspNetCore.Mvc;
 using BakedGoods.Models;
 using System.Collections.Generic;
@@ -6,10 +7,11 @@ namespace BakedGoods.Controllers
 {
   public class HomeController : Controller
   {
-
     [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.Pastries = _db.Pastries.ToList();
+      ViewBag.Flavors = _db.Flavors.ToList();
       return View();
     }
 

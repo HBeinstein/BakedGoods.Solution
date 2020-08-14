c1a54fa534d8147427071e9e0f14a082c806ebf3
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-08-14 15:46:40 -0700

    configure HomeController to use Entity

--- BakedGoods/Controllers/HomeController.cs
+++ BakedGoods/Controllers/HomeController.cs
@@ -2,11 +2,19 @@ using System.Reflection.PortableExecutable;
 using Microsoft.AspNetCore.Mvc;
 using BakedGoods.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BakedGoods.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly BakedGoodsContext _db;
+
+    public HomeController(BakedGoodsContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
